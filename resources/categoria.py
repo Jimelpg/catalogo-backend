420fb8b6693eb2f86e55da5506f43d295f603126
Mariana Neumann <mariana.neumann@upa.edu.py>
2022-09-19 11:49:31 -0400

    Swagger de categoria y modificacion en resorurces

--- resources/categoria.py
+++ resources/categoria.py
@@ -5,7 +5,7 @@ from utils import paginated_results,restrict
 from flask import request
 
 class CategoriaList (Resource):
-   ## @swag_from('../swagger/task/list_task.yaml')
+   ## @swag_from('../swagger/categoria/list_categoria.yaml')
     def get (self):
         query = CategoriaModel.query
         return paginated_results(query)
