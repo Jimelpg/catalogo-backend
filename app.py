4e53244278a60852e760dfb017b9f6996aab2c50
Jimena Lezcano <jimena.lezcano@upa.edu.py>
2022-09-20 10:54:43 -0400

    Cambios en app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, redirect
 from distutils.sysconfig import PREFIX
 from resources.producto import Producto, ProductoList, ProductoSearch
+from resources.categoria import CategoriaList
 from flask_restful import Api
 from flasgger import Swagger 
 import os 
@@ -56,6 +57,9 @@ def welcome():
 api.add_resource (Producto, f'{PREFIX}/productos/<id>') #definicion de un recurso
 api.add_resource (ProductoList, f'{PREFIX}/productos') #definicion de un recurso
 api.add_resource (ProductoSearch, f'{PREFIX}/search/productos')
+api.add_resource (CategoriaList, f'{PREFIX}/categoria')
+#AGREGAR DE PROVEEDORES
+
 
 # Bloque opcional para ejecutr con python app.py
 if __name__ =='__main__':
