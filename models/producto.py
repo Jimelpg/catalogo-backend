6a032db91469c3c8e4e806eca0efb90464fff1b0
Mariana Neumann <mariana.neumann@upa.edu.py>
2022-09-27 08:53:11 -0400

    cambios

--- models/producto.py
+++ models/producto.py
@@ -1,21 +1,33 @@
 #asi se define una clase en python ej "class nombre_clase"
 #agregarle atributos y de que tipo es
 #self es como el objeto del this
+from collections import UserList
 from db import db
 from flask_restful.reqparse import Namespace
 from utils import _assign_if_something
+from models.proveedor import ProveedorModel
+from models.categoria import CategoriaModel
 
 class ProductoModel(db.Model):
     __tablename__ = 'producto'
 
     id = db.Column(db.Integer, primary_key = True)
-    proveedor_id = db.Column(db.Integer)
-    categoria_id = db.Column(db.Integer)
+    proveedor_id = db.Column(db.Integer, db.ForeignKey(ProveedorModel.id))
+    categoria_id = db.Column(db.Integer, db.ForeignKey(CategoriaModel.id))
     nombre = db.Column(db.String)
     descrip = db.Column(db.String)
     precio = db.Column(db.Integer)
     estado = db.Column(db.String)
 
+    _proveedor = db.relationship('ProveedorModel', 
+    uselist = False, 
+    primaryjoin='ProveedorModel.id == ProductoModel.proveedor_id', 
+    foreign_keys='ProductoModel.proveedor_id')
+
+    _categoria = db.relationship('CategoriaModel', 
+    uselist = False, 
+    primaryjoin='CategoriaModel.id == ProductoModel.categoria_id', 
+    foreign_keys='ProductoModel.categoria_id')
 
 #se van a setear según los parametros asignados que se llaman igual 
     def __init__(self, id, proveedor_id, categoria_id, nombre, descrip, precio, estado):
@@ -28,7 +40,7 @@ class ProductoModel(db.Model):
         self.estado = estado
 
 #metodo para retomar el objeto en formato json o diccionario
-    def json(self, depht =0):
+    def json(self, depth =0):
         json = {
             'id': self.id,
             'proveedor_id': self.proveedor_id,
@@ -38,6 +50,11 @@ class ProductoModel(db.Model):
             'precio': self.precio,
             'estado': self.estado
         }
+        if depth >0:
+            if self._proveedor:
+                json['_proveedor'] = self._proveedor.json(depth)
+            if self._categoria:
+                json['_categoria'] = self._categoria.json(depth)
 
         return json
 
